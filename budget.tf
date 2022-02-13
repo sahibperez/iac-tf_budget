7daa14c1d1ddac63338e4a34c772660cb768735b
sahibperez <96750772+sahibperez@users.noreply.github.com>
2022-02-13 16:13:07 -0600

    Update budget.tf

--- budget.tf
+++ budget.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_budgets_budget" "sahib-budget" {
   name              = "monthly-budget-01"
   budget_type       = "COST"
-  limit_amount      = "50.0"
+  limit_amount      = "10.0"
   limit_unit        = "USD"
   time_unit         = "MONTHLY"
   time_period_start = "2021-12-01_00:01"
